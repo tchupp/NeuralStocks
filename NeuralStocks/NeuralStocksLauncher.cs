c59867cf483abef5210441b8e643eb3d41c9be55
tclchiam <tclchiam@gmail.com>
2015-05-30 22:39:03 -0400

    Removed random code from the main.

--- NeuralStocks/NeuralStocksLauncher.cs
+++ NeuralStocks/NeuralStocksLauncher.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using NeuralStocks.ApiCommunication;
-
-namespace NeuralStocks
+﻿namespace NeuralStocks
 {
     public class NeuralStocksLauncher : INeuralStocksLauncher
     {
         private static void Main(string[] args)
         {
-            var api = new StockMarketApi();
-            var lookup = api.CompanyLookup("AAPL");
-
-            Console.WriteLine(lookup);
         }
     }
 }
