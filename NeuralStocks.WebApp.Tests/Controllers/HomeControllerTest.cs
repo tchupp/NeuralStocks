2375aaa2165ab5f071556d1a456409a5356216c6
tclchiam <tclchiam@gmail.com>
2015-07-21 18:44:42 -0400

    Added new projects for the webapp! Completely empty for now

---
+++ NeuralStocks.WebApp.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Web.Mvc;
+using NeuralStocks.WebApp.Controllers;
+using NUnit.Framework;
+
+namespace NeuralStocks.WebApp.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void About()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [Test]
+        public void Contact()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ NeuralStocks.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NeuralStocks.WebApp.Startup))]
+namespace NeuralStocks.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
