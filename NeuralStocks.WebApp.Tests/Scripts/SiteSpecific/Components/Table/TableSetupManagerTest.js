8aa697cdfb593fe8ba7978474d9f556f2fa95a47
Theo Chupp <tclchiam@gmail.com>
2015-08-13 11:29:46 -0400

    When a user clicks on a row in the table it highlights.

--- NeuralStocks.WebApp.Tests/Scripts/SiteSpecific/Components/Table/TableSetupManagerTest.js
+++ NeuralStocks.WebApp.Tests/Scripts/SiteSpecific/Components/Table/TableSetupManagerTest.js
@@ -16,11 +16,19 @@ describe("TableSetupManagerTest", function() {
 
     describe("InitializeTable", function() {
         var $table;
+        var $tableBody;
+        var jQuerySelectorStub;
         beforeEach(function() {
-            $table = { DataTable: sinon.stub() };
+            $table = { DataTable: sinon.stub(), find: sinon.stub() };
+            $tableBody = { on: sinon.stub() };
+
+            $table.find.returns($tableBody);
+            jQuerySelectorStub = sinon.stub(window, "$");
+
         });
 
-        afterEach(function() {
+        afterEach(function () {
+            jQuerySelectorStub.restore();
         });
 
         it("testSetsUpDataTable_WithCorrectTableOptions", function() {
@@ -32,6 +40,51 @@ describe("TableSetupManagerTest", function() {
 
             assertTrue($table.DataTable.calledOnce);
             assertEquals(expectedArgs, $table.DataTable.getCall(0).args[0]);
+        });
+
+        it("testAddsOnClickToTableBody_WithCorrectArgs", function() {
+            assertFalse($table.find.called);
+            assertFalse($tableBody.on.called);
+
+            TableSetupManager.initializeTable($table, {});
+
+            assertEquals(1, $table.find.callCount);
+            assertEquals(1, $table.find.getCall(0).args.length);
+            assertEquals("tbody", $table.find.getCall(0).args[0]);
+
+            assertEquals(1, $tableBody.on.callCount);
+            assertEquals(3, $tableBody.on.getCall(0).args.length);
+            assertEquals("click", $tableBody.on.getCall(0).args[0]);
+            assertEquals("tr", $tableBody.on.getCall(0).args[1]);
+        });
+
+        it("testAddsOnClickToTableBody_ClickTogglesTrClass", function () {
+            assertFalse($table.find.called);
+            assertFalse($tableBody.on.called);
+
+            TableSetupManager.initializeTable($table, {});
+
+            assertEquals(1, $tableBody.on.callCount);
+            assertEquals(3, $tableBody.on.getCall(0).args.length);
+
+            var clickCallback = $tableBody.on.getCall(0).args[2];
+
+            var mockTableRow = { clickCallback: clickCallback };
+            var $mockTableRow = { toggleClass: sinon.stub() };
+            jQuerySelectorStub.returns($mockTableRow);
+
+            assertFalse(jQuerySelectorStub.called);
+            assertFalse($mockTableRow.toggleClass.called);
+
+            mockTableRow.clickCallback();
+
+            assertEquals(1, jQuerySelectorStub.callCount);
+            assertEquals(1, jQuerySelectorStub.getCall(0).args.length);
+            assertEquals(mockTableRow, jQuerySelectorStub.getCall(0).args[0]);
+
+            assertEquals(1, $mockTableRow.toggleClass.callCount);
+            assertEquals(1, $mockTableRow.toggleClass.getCall(0).args.length);
+            assertEquals("selected", $mockTableRow.toggleClass.getCall(0).args[0]);
         });
     });
 });
--- NeuralStocks.WebApp/Scripts/SiteSpecific/Components/Table/TableSetupManager.js
+++ NeuralStocks.WebApp/Scripts/SiteSpecific/Components/Table/TableSetupManager.js
@@ -3,6 +3,10 @@
 
     modualTableSetupManager.initializeTable = function ($table, tableOptions) {
         $table.DataTable(tableOptions);
+
+        $table.find("tbody").on("click", "tr", function () {
+            $(this).toggleClass("selected");
+        });
     };
 
     return modualTableSetupManager;
